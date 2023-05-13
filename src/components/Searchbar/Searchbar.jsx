8542a7a46213734e4ad3c0bc44097a77c08ad763
bakmaiev <thisyoungrhyme@gmail.com>
2023-05-13 22:03:06 +0300

    Add new changes

---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+
+class Searchbar extends Component {
+  state = {
+    imagesName: '',
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    if (this.state.imagesName.trim() === '') {
+      alert('Ahtung! You can add something');
+      return;
+    }
+
+    this.props.onSubmit(this.state.imagesName);
+    this.setState({ imagesName: '' });
+  };
+
+  handleChangeName = e => {
+    this.setState({ imagesName: e.currentTarget.value.toLowerCase() });
+  };
+
+  render() {
+    return (
+      <header className="searchbar">
+        <form className="form" onSubmit={this.handleSubmit}>
+          <button type="submit" className="button">
+            <span className="button-label">Search</span>
+          </button>
+
+          <input
+            className="input"
+            type="text"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            value={this.state.imagesName}
+            onChange={this.handleChangeName}
+          />
+        </form>
+      </header>
+    );
+  }
+}
+
+export default Searchbar;
