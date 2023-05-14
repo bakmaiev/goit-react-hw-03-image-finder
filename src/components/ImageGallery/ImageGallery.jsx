4e177ad5ad5c5758a42e5f30f33580915888989b
bakmaiev <thisyoungrhyme@gmail.com>
2023-05-14 22:24:34 +0300

    Add new changes

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,14 +1,20 @@
 import { fetchCard } from 'API';
 import ImageGalleryItem from 'components/ImageGalleryItem/ImageGalleryItem';
+import { Loader } from 'components/Loader';
 import React, { Component } from 'react';
+import { toast } from 'react-toastify';
 
 class ImageGallery extends Component {
   state = {
     data: null,
+    isLoading: false,
+    status: 'idle',
   };
 
   componentDidUpdate(prevProps) {
     if (this.props.imageName === prevProps.imageName) return;
+    this.setState({ data: null });
+    this.setState({ isLoading: true });
     this.getCard();
   }
 
@@ -17,49 +23,33 @@ class ImageGallery extends Component {
       const { data } = await fetchCard(this.props.imageName);
       this.setState({ data: data.hits });
       console.log(data.hits);
-
-      // if (!data.hits.length) {
-      //   galleryListEl.innerHTML = '';
-      //   return Notiflix.Notify.failure(
-      //     'Sorry, there are no images matching your search query. Please try again.'
-      //   );
-      // }
-      // galleryListEl.insertAdjacentHTML('beforeend', createMarkup(data.hits));
-      // loadMoreBtnEl.classList.remove('visually-hidden');
-      // pixabaiAPI.setTotal(data.totalHits);
-      // lightbox.refresh();
-      // return Notiflix.Notify.success(
-      //   `Done! We found ${data.totalHits} images.`
-      // );
     } catch (err) {
       console.log(err);
+      toast.error(err.message);
+    } finally {
+      this.setState({ isLoading: false });
     }
-    //   finally {
-    //   if (!pixabaiAPI.hasMoreImages()) {
-    //     loadMoreBtnEl.classList.add('visually-hidden');
-    //     Notiflix.Notify.info(
-    //       "We're sorry, but you've reached the end of search results."
-    //     );
-    //   }
-    // }
   };
   render() {
-    const { data } = this.state;
+    const { data, isLoading } = this.state;
     const name = this.props.imageName;
     return (
-      <ul className="gallery">
-        {data &&
-          data.map(el => {
-            return (
-              <ImageGalleryItem
-                key={el.id}
-                webformatURL={el.webformatURL}
-                largeImageURL={el.largeImageURL}
-                name={name}
-              />
-            );
-          })}
-      </ul>
+      <>
+        {isLoading && <Loader />}
+        <ul className="gallery">
+          {data &&
+            data.map(el => {
+              return (
+                <ImageGalleryItem
+                  key={el.id}
+                  webformatURL={el.webformatURL}
+                  largeImageURL={el.largeImageURL}
+                  name={name}
+                />
+              );
+            })}
+        </ul>
+      </>
     );
   }
 }
